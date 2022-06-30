827ea9b44a94bc7e526905aa1db8ace4db2e1f2d
Vytautas-T <105976888+Vytautas-T@users.noreply.github.com>
2022-06-30 12:02:39 +0300

    pirmas uzdavinis pabaiktas

--- src/pirmaUzduotis.java
+++ src/pirmaUzduotis.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 public class pirmaUzduotis {
     public static void main(String[] args) {
         Scanner skaitytuvas = new Scanner(System.in);
-        System.out.println("Irasikite skaiciu");
-        int a = skaitytuvas.nextInt();
-        System.out.println(arPirminisSkaicius(a));
+       // System.out.println("Irasikite skaiciu");
+        //int a = skaitytuvas.nextInt();
+        //System.out.println(arPirminisSkaicius(a));
         int array[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-
+        System.out.println(pirmujuKiekisMasyve(array));
     }
     public  static boolean arPirminisSkaicius(int skaicius){
         boolean pirminis = true;
@@ -19,7 +19,13 @@ public class pirmaUzduotis {
         return pirminis;
     }
 
-    public static int pirmujuKiekisMasyve(){
-        if (arPirminisSkaicius())
+    public static int pirmujuKiekisMasyve(int[] array){
+        int skaicius = 0;
+        for (int i = 0; i < array.length; ++i) {
+            if (!arPirminisSkaicius(i)){
+                skaicius++;
+            }
+        }
+        return skaicius;
     }
 }
